8e6bc7e0f98dceed1bf95197925ed0ae7803defb
PARAKH CHOUDHARY <98397497+parakhchoudhary1208@users.noreply.github.com>
2022-07-12 00:18:32 +0530

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,13 @@
+function insta() {
+    window.open("https://www.instagram.com/multiverse_artist/","_blank");
+
+}
+function linkedin() {
+    window.open("https://www.linkedin.com/in/parakh-choudhary-a6ba81175/","_blank");
+
+}
+function github() {
+    window.open("https://github.com/parakhchoudhary1208","_blank");
+
+}
+
